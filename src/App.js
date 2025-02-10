1e481feab37e1adb7b38162b35221a7206952047
Ahmed Frag <a.frag166@gmail.com>
2025-02-10 13:11:56 +0200

    Defining Routes as Objects of Array to createBrowserRouter() method

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {path: '/Products', element: <ProductsPage />}
+])
+
 function App() {
   return <div></div>;
 }
