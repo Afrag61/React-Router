f0f3123cd6b62f9ec42ec50374339bbe8e6bc813
Ahmed Frag <a.frag166@gmail.com>
2025-02-10 13:15:13 +0200

    Providing routes by (RouterProvider component) to the App component because app is Entry point of my react app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
