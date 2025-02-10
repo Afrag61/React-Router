c72ee7aa1cdd4c41b8801d67c2fcaffbcbf2c791
Ahmed Frag <a.frag166@gmail.com>
2025-02-10 13:24:21 +0200

    Navigating between Pages with Links (Link component)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 
+// const routerDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/Products' element={<ProductsPage />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routerDefinition)
+
 const router = createBrowserRouter([
   {path: '/', element: <HomePage />},
   {path: '/Products', element: <ProductsPage />}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,10 @@
+import { Link } from 'react-router-dom'
+
 const HomePage = () => {
-  return <h1>My Home Page</h1>;
+  return <>
+    <h1>My Home Page</h1>
+    <p>Go to <Link to='/products'>the list of Products</Link></p>
+  </>;
 };
 
 export default HomePage;
